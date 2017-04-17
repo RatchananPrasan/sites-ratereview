8b6701533cb93325c19b4c7ea033dd15c8b0bfd8
virayuts <virayuts@gmail.com>
2017-04-18 05:49:34 +0700

    register submit button effects

--- accounts/static/accounts/scripts/register.js
+++ accounts/static/accounts/scripts/register.js
@@ -1,33 +1,47 @@
+var check_username = false;
+var check_password1 = false;
+var check_password2 = false;
+var check_email = false;
+
 function validate(obj) {
     var object_type = obj.attr('type');
     var text_input = obj.val();
     if (object_type == 'password' && obj.attr('name') == 'password2') {
         if (text_input != $('#password1').val() || text_input.length < 8 || text_input.includes(' ')) {
+            check_password2 = false;
             return false;
         } else {
+            check_password2 = true;
             return true;
         }
     }
     if (object_type == 'text' || object_type == 'password') {
-        if (object_type == 'password') {
-            $('#password2').trigger('change');
-        }
+        var result;
         if (text_input.length < 8 || text_input.includes(' ')) {
-            return false;
+            result = false;
         } else {
-            return true;
+            result = true;
         }
+
+        if (object_type == 'password') {
+            check_password1 = result;
+            $('#password2').trigger('change');
+        } else {
+            check_username = result;
+        }
+        return result;
     } else if (object_type == 'email') {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(text_input);
+        check_email = re.test(text_input);
+        return check_email;
     }
 }
 
 
 $(document).ready(function () {
-    
+
     $('#register-container').fadeIn('slow');
-    
+
     var $ok_span = $('<span class="glyphicon glyphicon-ok form-control-feedback removable" aria-hidden="true"></span>')
     var $remove_span = $('<span class="glyphicon glyphicon-remove form-control-feedback removable" aria-hidden="true"></span>')
 
@@ -63,5 +77,26 @@ $(document).ready(function () {
                 hidden.removeClass('hidden');
             }
         }
+    });
+
+    var $form_group = $('.form-group');
+    $('button[type="submit"]').on('click', function () {
+        if (check_username && check_password1 && check_password2 && check_email) {
+            return true;
+        }
+        
+        if (!check_username) {
+            $form_group.eq(0).effect('shake', 'slow');
+        }
+        if (!check_password1) {
+            $form_group.eq(1).effect('shake', 'slow');
+        }
+        if (!check_password2) {
+            $form_group.eq(2).effect('shake', 'slow');
+        }
+        if (!check_email) {
+            $form_group.eq(3).effect('shake', 'slow');
+        }
+        return false;
     })
 });
