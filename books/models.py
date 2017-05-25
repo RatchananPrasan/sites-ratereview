8fd5134bb683c042a2f56fa675c1cdd50cb9aa50
Charnchyyy <dhitiwat.jc@gmail.com>
2017-05-25 17:07:28 +0700

    upload books app

--- books/models.py
+++ books/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models import Avg
+from accounts.models import User
+from django.utils import timezone
+
+
 
 # Create your models here.       
 
@@ -6,10 +11,28 @@ class Author(models.Model):
     author_name = models.CharField(max_length = 50)
 
 class Book(models.Model):
-    GENRE = ('fiction','non-fiction', 'other')
-    isbn = models.CharField(max_length = 13, primary_key = True)
+    GENRE = (('arts','Arts & Photography'),('biographies','Biographies & Memoirs'),('children','Children\'s Books'),('histosy','History'),('literature','Literature & Fiction'),('mystery','Mystery & Suspense'), ('romance','Romance'),('sci-fi','Sci-Fi & Fiction'),('teens','Teens & Young Adult'),('textbook','Textbooks'),('other','Other'))
+    isbn = models.CharField(max_length = 13)
     book_name = models.CharField(max_length = 50)
     author = models.ForeignKey(Author,on_delete = models.CASCADE)
-    #genre = models.CharField(max_length = 20, choices = GENRE)
+    genre = models.CharField(max_length = 20, choices = GENRE, default = ('other','Other'))
+    book_cover = models.ImageField(upload_to = 'media/books/book_cover',default = 'media/books/book_cover/no_cover.jpg')
+    
+    booked_by = models.ForeignKey(User,on_delete = models.CASCADE, related_name = 'booked_by')
+    
+    def getRating(self):
+        rating = BookRating.objects.filter(book = self.id).aggregate(Avg('rate'))
+        rate = rating['rate__avg']
+        if rate == None:
+            rate = 0
+        return rate
 
+
+class BookRating(models.Model):
+    RATE = ((1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'))
+    rate = models.IntegerField(choices = RATE)
+    description = models.TextField()
+    book = models.ForeignKey(Book,on_delete = models.CASCADE)
+    booked_by = models.ForeignKey(User,on_delete = models.CASCADE, related_name = 'booked_rated_by')
+    pub_date = models.DateTimeField(default=timezone.now)
 
--- books/views.py
+++ books/views.py
@@ -1,22 +1,111 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 
 from .forms import CreateBookForm
-
+from .models import *
+from .validateFormatTools import VFBookTool
 # Create your views here.
 
 
-def get_book_name(request):
-    # if this is a POST request we need to process the form data
-    if request.methis == 'POST':
-        # creatr e a form instance and populate it with data from the request:
-        form = CreateBookForm(request.POST)
-        # check whether it's valid:
+
+def createNewBook(request):
+    if request.method == 'POST':
+        form = CreateBookForm(request.POST,request.FILES)
         if form.is_valid():
-            #process the data in form.cleaned_Data as required
-            #...
-            #redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-        else:
-            form = BookForm()
-        return render(request, 'create_book.html', {'form': form})
+            #print(form.cleaned_data)
+            input_ISBN = form.cleaned_data['isbn']
+            input_book_name = form.cleaned_data['book_name']
+            input_author_name = form.cleaned_data['author']
+            input_genre = form.cleaned_data['genre']
+            input_book_cover = form.cleaned_data['book_cover']
+            wizard = VFBookTool()
+            if wizard.validateISBN(input_ISBN):
+                input_ISBN = wizard.formatISBN(input_ISBN)
+                try:
+                    book = Book.objects.get(isbn = input_ISBN)
+                    #return render(request, 'books/create_book.html',{'form':form,'error':'Book with the following ISBN already exist'})
+                    return redirect('books:book_detail',book.id)
+                except Book.DoesNotExist:
+                    try:
+                        input_author_name = wizard.formatAuthorName(input_author_name)
+                        writer = Author.objects.get(author_name = input_author_name )
+                    except Author.DoesNotExist:
+                        writer = Author(author_name = input_author_name)
+                        writer.save()
+                    #CreateBook
+                    newBook = Book(isbn = input_ISBN, book_name = input_book_name, author = writer, genre = input_genre, book_cover = input_book_cover, booked_by = request.user )
+                    newBook.save()
+                    
+                    return redirect('books:book_detail',newBook.id)
+                
+            else:
+                form = CreateBookForm()
+                return render(request, 'books/create_book.html',{'form':form,'error':'ISBN must contain only number'})
+            
+    form = CreateBookForm()
+    #return render(request, 'books/create_book.html',{'form':form})
+    return render(request, 'books/createBook.html',{'form':form})
+
+
+def get_book_detail(request,book_id):
+    book = get_object_or_404(Book, pk = book_id)
+  
+        
+    
+    try:
+        ## get post
+        ratedPost = BookRating.objects.filter(book = book.id).order_by('-pub_date')
+        print('\t\t',ratedPost)
+        return render(request,'books/book_detail.html',{'book':book,'ratedPost':ratedPost})
+    except:
+        return render(request,'books/book_detail.html',{'book':book,'ratedPost':None})
+
+
+def searching(request):
+    s = request.GET['search_key']
+    sk = "+".join(s.split())
+    if sk != '':
+        return redirect('books:book_search',sk)
+    else:
+        books = Book.objects.all()
+        return render(request, 'books/browse_page.html',{'books':books})
+    
+
+def searchBook(request,search_key):
+    search_key = " ".join(search_key.split('+'))
+    books = Book.objects.filter(book_name__icontains =  search_key)| Book.objects.filter(author__author_name__icontains = search_key)
+    return render(request, 'books/browse_page.html',{'books':books, 'book_found':len(books)})
+
+
+def browseAllBooks(request):
+    books = Book.objects.all()
+    print(books)
+    return render(request, 'books/browse_page.html',{'books':books, 'book_found':len(books)})
+
+
+def createNewBookRating(request,book_id):
+      
+    if request.method == 'POST':      
+        print('\tin')
+        try:
+            rating = request.POST.get('rated_score')
+            if rating is None:
+                raise ValueError
+            comment = request.POST.get('comment')
+            
+            print(rating,"star")
+            print(comment)
+            print(request.user)
+            book = Book.objects.get(pk = book_id)
+            newBookRate = BookRating(rate = rating, description = comment, book = book ,booked_by = request.user)
+            newBookRate.save()
+            
+            
+        except ValueError:
+            print('\t\terror')
+            pass
+        
+    return redirect('books:book_detail',book_id)
+        
+
+                                
